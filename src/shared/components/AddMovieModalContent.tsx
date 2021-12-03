3db724702399d7a86bce24fd2647243ea6c24350
filipHartman <filip.hartman23@gmail.com>
2021-12-03 13:03:56 +0100

    task-7 add formik to project

--- src/shared/components/AddMovieModalContent.tsx
+++ src/shared/components/AddMovieModalContent.tsx
@@ -1,32 +1,93 @@
+import { Field, useFormik } from 'formik';
 import { ModalProps } from '.';
+import { Genre, Movie } from '../interfaces';
+
+const initialValues: Movie = {
+  title: '',
+  tagline: '',
+  vote_average: 0,
+  vote_count: 0,
+  release_date: '',
+  poster_path: '',
+  overview: '',
+  budget: 0,
+  revenue: 0,
+  runtime: 0,
+  genres: [],
+  id: 0,
+};
 
 export const AddMovieModalContent = (props: ModalProps) => {
+  const genres = Genre;
+  const formik = useFormik<Movie>({
+    initialValues,
+    onSubmit: (value) => {
+      console.log('Value ', value);
+      props.close();
+    },
+    onReset: (values) => {
+      values = initialValues;
+    },
+  });
   return (
-    <>
+    <form onSubmit={formik.handleSubmit}>
       <div>
         <label htmlFor='title'>Title</label>
-        <input id='title' />
+        <input
+          id='title'
+          name='title'
+          onChange={formik.handleChange}
+          value={formik.values.title}
+        />
         <label htmlFor='release-date'>release date</label>
-        <input id='release-date' />
+        <input
+          id='release-date'
+          name='release_date'
+          type='date'
+          onChange={formik.handleChange}
+          value={formik.values.release_date}
+        />
       </div>
       <div>
-        <label htmlFor='movie-url'>Movie Url</label>
-        <input id='movie-url' />
+        <label htmlFor='poster_path'>Poster path</label>
+        <input
+          id='poster_path'
+          name='poster_path'
+          type='url'
+          onChange={formik.handleChange}
+          value={formik.values.poster_path}
+        />
         <label htmlFor='rating'>Rating</label>
-        <input id='rating' />
+        <input
+          id='rating'
+          name='rating'
+          type='number'
+          onChange={formik.handleChange}
+          value={formik.values.vote_average}
+        />
       </div>
       <div>
-        <label htmlFor='genre'>Movie Url</label>
-        <input id='genre' />
+        <label htmlFor='genres'>Select genre</label>
+
+        <Field component='select' id='genres' name='genres' multiple={true}>
+          {Object.values(genres).map((genre, i) => (
+            <option key='i' value={genre}>
+              {genre}
+            </option>
+          ))}
+        </Field>
+
         <label htmlFor='runtime'>Runtime</label>
         <input id='runtime' />
       </div>
       <label htmlFor='overview'>Over view</label>
       <input id='overview' />
       <div>
-        <button>Reset</button>
-        <button onClick={() => props.close()}>Submit</button>
+        <button onClick={formik.handleReset} type='reset'>
+          Reset
+        </button>
+        <button type='submit'>Submit</button>
       </div>
-    </>
+    </form>
   );
 };
--- src/shared/interfaces.ts
+++ src/shared/interfaces.ts
@@ -26,7 +26,7 @@ export interface Movie {
   tagline: string;
   vote_average: number;
   vote_count: number;
-  release_date: Date;
+  release_date: string;
   poster_path: string;
   overview: string;
   budget: number;
